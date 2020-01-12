f4acc5392e152e61f436da03af5578eed3c60d6a
Saitgalin Aleksandr <cred1tor@yandex.ru>
2020-01-12 12:34:19 +0600

    added arguments and -f option to help display, make run calls help directly

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import program from 'commander';
 import { version } from '../package.json';
 
-program.version(version);
-program.description('Compares two configuration files and shows a difference.');
+program.version(version)
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<firstConfig> <secondConfig>')
+  .option('-f, --format [type]', 'output format');
 
 export default () => program.parse(process.argv);

df350f2bbaf7a8bc83ad6b0607e1b71ed5fe06d0
Saitgalin Aleksandr <cred1tor@yandex.ru>
2020-01-12 22:26:44 +0600

    fixed the test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,7 +1,7 @@
 import genDiff from '../src';
 
 test('genDiff', () => {
-  const diff = genDiff('/test-files/before.json', '/test-files/after.json');
+  const diff = genDiff('__tests__/test-files/before.json', '__tests__/test-files/after.json');
   const lines = diff.split('\n').length;
   expect(lines).toBe(8);
 });

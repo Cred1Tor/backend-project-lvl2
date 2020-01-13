12be0818e4e69c61eebc714a194ba62d517cc60a
Saitgalin Aleksandr <cred1tor@yandex.ru>
2020-01-13 21:49:52 +0600

    moved __fixtures__ to root

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,10 +3,10 @@ import path from 'path';
 import genDiff from '../src';
 
 test('genDiff', () => {
-  const path1 = path.join(__dirname, '/__fixtures__/before.json');
-  const path2 = path.join(__dirname, '/__fixtures__/after.json');
+  const path1 = path.join(__dirname, '../__fixtures__/before.json');
+  const path2 = path.join(__dirname, '../__fixtures__/after.json');
   const diff = genDiff(path1, path2);
-  const path3 = path.join(__dirname, '/__fixtures__/expected');
+  const path3 = path.join(__dirname, '../__fixtures__/expected');
   const expected = fs.readFileSync(path3, 'utf-8');
   expect(diff).toEqual(expected);
 });

2fdd39be7c7b12ba6c1e432e4e42b22f55a07b32
Saitgalin Aleksandr <cred1tor@yandex.ru>
2020-01-13 18:25:22 +0600

    updated the test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,14 @@
 import genDiff from '../src';
 
 test('genDiff', () => {
-  const diff = genDiff('__tests__/test-files/before.json', '__tests__/test-files/after.json');
-  const lines = diff.split('\n').length;
-  expect(lines).toBe(8);
+  const diff = genDiff('__tests__/__fixtures__/before.json', '__tests__/__fixtures__/after.json');
+  const expected = `{
+    host: hexlet.io
+  - timeout: 50
+  + timeout: 20
+  - proxy: 123.234.53.22
+  - follow: false
+  + verbose: true
+}`;
+  expect(diff).toEqual(expected);
 });

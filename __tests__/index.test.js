73e1b91f454a969afb0567ac082f902371b6b66c
Saitgalin Aleksandr <cred1tor@yandex.ru>
2020-01-14 21:32:11 +0600

    added a test for yaml files

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,11 +2,22 @@ import fs from 'fs';
 import path from 'path';
 import genDiff from '../src';
 
-test('genDiff', () => {
-  const path1 = path.join(__dirname, '../__fixtures__/before.json');
-  const path2 = path.join(__dirname, '../__fixtures__/after.json');
-  const diff = genDiff(path1, path2);
-  const path3 = path.join(__dirname, '../__fixtures__/expected');
-  const expected = fs.readFileSync(path3, 'utf-8');
-  expect(diff).toEqual(expected);
+describe('gendiff', () => {
+  test('json', () => {
+    const path1 = path.join(__dirname, '../__fixtures__/before.json');
+    const path2 = path.join(__dirname, '../__fixtures__/after.json');
+    const diff = genDiff(path1, path2);
+    const path3 = path.join(__dirname, '../__fixtures__/expected');
+    const expected = fs.readFileSync(path3, 'utf-8');
+    expect(diff).toEqual(expected);
+  });
+
+  test('yaml', () => {
+    const path1 = path.join(__dirname, '../__fixtures__/before.yml');
+    const path2 = path.join(__dirname, '../__fixtures__/after.yml');
+    const diff = genDiff(path1, path2);
+    const path3 = path.join(__dirname, '../__fixtures__/expected');
+    const expected = fs.readFileSync(path3, 'utf-8');
+    expect(diff).toEqual(expected);
+  });
 });

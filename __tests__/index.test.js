9da0f20a0f2dd2d6ff10fc7a7715dcdb0473dd77
Saitgalin Aleksandr <cred1tor@yandex.ru>
2020-01-14 21:51:25 +0600

    cleanup

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,23 +1,21 @@
 import fs from 'fs';
-import path from 'path';
 import genDiff from '../src';
 
 describe('gendiff', () => {
+  const expectedPath = `${__dirname}/../__fixtures__/expected`;
+  const expected = fs.readFileSync(expectedPath, 'utf-8');
+
   test('json', () => {
-    const path1 = path.join(__dirname, '../__fixtures__/before.json');
-    const path2 = path.join(__dirname, '../__fixtures__/after.json');
+    const path1 = `${__dirname}/../__fixtures__/before.json`;
+    const path2 = `${__dirname}/../__fixtures__/after.json`;
     const diff = genDiff(path1, path2);
-    const path3 = path.join(__dirname, '../__fixtures__/expected');
-    const expected = fs.readFileSync(path3, 'utf-8');
     expect(diff).toEqual(expected);
   });
 
   test('yaml', () => {
-    const path1 = path.join(__dirname, '../__fixtures__/before.yml');
-    const path2 = path.join(__dirname, '../__fixtures__/after.yml');
+    const path1 = `${__dirname}/../__fixtures__/before.yml`;
+    const path2 = `${__dirname}/../__fixtures__/after.yml`;
     const diff = genDiff(path1, path2);
-    const path3 = path.join(__dirname, '../__fixtures__/expected');
-    const expected = fs.readFileSync(path3, 'utf-8');
     expect(diff).toEqual(expected);
   });
 });

66cbf03561c720dc02ec379e28bb9773934f3246
Saitgalin Aleksandr <cred1tor@yandex.ru>
2020-01-15 22:20:28 +0600

    added ini parser

--- src/parsers.js
+++ src/parsers.js
@@ -1,10 +1,12 @@
 import path from 'path';
 import yaml from 'js-yaml';
 import fs from 'fs';
+import ini from 'ini';
 
 const mapping = {
   '.json': JSON.parse,
   '.yml': yaml.safeLoad,
+  '.ini': ini.parse,
 };
 
 export default (filepath) => {

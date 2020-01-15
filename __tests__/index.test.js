f4c6fe1ec1813b73356293cfc0b2ea54c9f43fec
Saitgalin Aleksandr <cred1tor@yandex.ru>
2020-01-15 22:52:19 +0600

    added test.each() and getFixturePath()

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,28 +1,19 @@
 import fs from 'fs';
 import genDiff from '../src';
 
+const getFixturePath = (fixtureName) => `${__dirname}/../__fixtures__/${fixtureName}`;
+
 describe('gendiff', () => {
-  const expectedPath = `${__dirname}/../__fixtures__/expected`;
-  const expected = fs.readFileSync(expectedPath, 'utf-8');
+  const expected = fs.readFileSync(getFixturePath('expected'), 'utf-8');
 
-  test('json', () => {
-    const path1 = `${__dirname}/../__fixtures__/before.json`;
-    const path2 = `${__dirname}/../__fixtures__/after.json`;
-    const diff = genDiff(path1, path2);
-    expect(diff).toEqual(expected);
-  });
-
-  test('yaml', () => {
-    const path1 = `${__dirname}/../__fixtures__/before.yml`;
-    const path2 = `${__dirname}/../__fixtures__/after.yml`;
-    const diff = genDiff(path1, path2);
-    expect(diff).toEqual(expected);
-  });
-
-  test('ini', () => {
-    const path1 = `${__dirname}/../__fixtures__/before.ini`;
-    const path2 = `${__dirname}/../__fixtures__/after.ini`;
-    const diff = genDiff(path1, path2);
+  test.each([
+    ['json', 'before.json', 'after.json'],
+    ['yaml', 'before.yml', 'after.yml'],
+    ['ini', 'before.ini', 'after.ini'],
+  ])('%s', (_testName, beforeFileName, afterFileName) => {
+    const beforePath = getFixturePath(beforeFileName);
+    const afterPath = getFixturePath(afterFileName);
+    const diff = genDiff(beforePath, afterPath);
     expect(diff).toEqual(expected);
   });
 });

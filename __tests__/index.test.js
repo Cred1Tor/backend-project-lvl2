5a5b9087a98615b114ca9713c695bcf9ca6b90aa
Saitgalin Aleksandr <cred1tor@yandex.ru>
2020-01-18 20:28:46 +0600

    added a test for nested json

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,13 +3,28 @@ import genDiff from '../src';
 
 const getFixturePath = (fixtureName) => `${__dirname}/../__fixtures__/${fixtureName}`;
 
-describe('gendiff', () => {
-  const expected = fs.readFileSync(getFixturePath('expected'), 'utf-8');
+describe('gendiff flat', () => {
+  const expected = fs.readFileSync(getFixturePath('diff-flat'), 'utf-8');
 
   test.each([
     ['json', 'before.json', 'after.json'],
     ['yaml', 'before.yml', 'after.yml'],
     ['ini', 'before.ini', 'after.ini'],
+  ])('%s', (_testName, beforeFileName, afterFileName) => {
+    const beforePath = getFixturePath(beforeFileName);
+    const afterPath = getFixturePath(afterFileName);
+    const diff = genDiff(beforePath, afterPath);
+    expect(diff).toEqual(expected);
+  });
+});
+
+describe('gendiff nested', () => {
+  const expected = fs.readFileSync(getFixturePath('diff-nested'), 'utf-8');
+
+  test.each([
+    ['json', 'before-nested.json', 'after-nested.json'],
+    // ['yaml', 'before.yml', 'after.yml'],
+    // ['ini', 'before.ini', 'after.ini'],
   ])('%s', (_testName, beforeFileName, afterFileName) => {
     const beforePath = getFixturePath(beforeFileName);
     const afterPath = getFixturePath(afterFileName);

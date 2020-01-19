3049db84dee237d80ad7b223ad0cb36e503240ef
Saitgalin Aleksandr <cred1tor@yandex.ru>
2020-01-19 10:35:43 +0600

    added tests for nested ini's

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,9 +3,6 @@ import genDiff from '../src';
 
 const getFixturePath = (fixtureName) => `${__dirname}/../__fixtures__/${fixtureName}`;
 
-// const afterNestedJson = JSON.parse(fs.readFileSync(getFixturePath('after-nested.json'), 'utf-8'));
-// console.log(yaml.safeDump(afterNestedJson));
-
 describe('gendiff flat', () => {
   const expected = fs.readFileSync(getFixturePath('diff-flat'), 'utf-8');
 
@@ -27,7 +24,7 @@ describe('gendiff nested', () => {
   test.each([
     ['json', 'before-nested.json', 'after-nested.json'],
     ['yaml', 'before-nested.yml', 'after-nested.yml'],
-    // ['ini', 'before.ini', 'after.ini'],
+    ['ini', 'before-nested.ini', 'after-nested.ini'],
   ])('%s', (_testName, beforeFileName, afterFileName) => {
     const beforePath = getFixturePath(beforeFileName);
     const afterPath = getFixturePath(afterFileName);

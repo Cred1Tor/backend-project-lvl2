c572846f631e998e81f7cab7ff9d25ea99e9989a
Saitgalin Aleksandr <cred1tor@yandex.ru>
2020-01-19 23:46:22 +0600

    added tests for new genDiff implementation

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -33,14 +33,23 @@ describe('gendiff nested', () => {
   });
 });
 
-describe('giff data', () => {
-  test('flat', () => {
-    const beforePath = getFixturePath('before.json');
-    const afterPath = getFixturePath('after.json');
-    const data1 = JSON.parse(fs.readFileSync(beforePath));
-    const data2 = JSON.parse(fs.readFileSync(afterPath));
+describe('diff data', () => {
+  test.each([
+    ['flat', 'before.json', 'after.json', 'diff-structure.json'],
+    ['nested', 'before-nested.json', 'after-nested.json', 'nested-diff-structure.json'],
+  ])('%s', (_testName, beforeFileName, afterFileName, expectedFileName) => {
+    const beforePath = getFixturePath(beforeFileName);
+    const afterPath = getFixturePath(afterFileName);
+    const data1 = JSON.parse(fs.readFileSync(beforePath), 'utf-8');
+    const data2 = JSON.parse(fs.readFileSync(afterPath), 'utf-8');
     const diffData = getDiffData(data1, data2);
-    const expected = JSON.parse(fs.readFileSync(getFixturePath('diff-structure.json')));
+    const expectedPath = getFixturePath(expectedFileName);
+    const expected = JSON.parse(fs.readFileSync(expectedPath, 'utf-8'));
     expect(diffData).toEqual(expected);
   });
 });
+
+const beforePath = getFixturePath('before-nested.json');
+const afterPath = getFixturePath('after-nested.json');
+const diff = genDiff(beforePath, afterPath);
+console.log(diff);

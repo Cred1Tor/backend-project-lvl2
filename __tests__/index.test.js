17011ae4dfad19ac108163d39fb3c95adb91c98d
Saitgalin Aleksandr <cred1tor@yandex.ru>
2020-01-21 22:51:01 +0600

    added tests for plain format, changed status names, removed flat tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,24 +1,9 @@
 import fs from 'fs';
-import genDiff, { getOrderedDiff } from '../src';
+import genDiff, { getDiff } from '../src';
 
 const getFixturePath = (fixtureName) => `${__dirname}/../__fixtures__/${fixtureName}`;
 
-describe('gendiff flat', () => {
-  const expected = fs.readFileSync(getFixturePath('diff-flat'), 'utf-8');
-
-  test.each([
-    ['json', 'before.json', 'after.json'],
-    ['yaml', 'before.yml', 'after.yml'],
-    ['ini', 'before.ini', 'after.ini'],
-  ])('%s', (_testName, beforeFileName, afterFileName) => {
-    const beforePath = getFixturePath(beforeFileName);
-    const afterPath = getFixturePath(afterFileName);
-    const diff = genDiff(beforePath, afterPath);
-    expect(diff).toEqual(expected);
-  });
-});
-
-describe('gendiff nested', () => {
+describe('tree', () => {
   const expected = fs.readFileSync(getFixturePath('diff-nested'), 'utf-8');
 
   test.each([
@@ -33,24 +18,37 @@ describe('gendiff nested', () => {
   });
 });
 
-describe('diff data', () => {
+describe('plain', () => {
+  const expected = fs.readFileSync(getFixturePath('plain-diff.txt'), 'utf-8');
+
   test.each([
-    ['ordered', getOrderedDiff, 'before-nested.json', 'after-nested.json', 'nested-ordered-diff.json'],
-  ])('%s', (_testName, getDiffData, beforeFileName, afterFileName, expectedFileName) => {
+    ['json', 'before-nested.json', 'after-nested.json'],
+    ['yaml', 'before-nested.yml', 'after-nested.yml'],
+    ['ini', 'before-nested.ini', 'after-nested.ini'],
+  ])('%s', (_testName, beforeFileName, afterFileName) => {
     const beforePath = getFixturePath(beforeFileName);
     const afterPath = getFixturePath(afterFileName);
-    const expectedPath = getFixturePath(expectedFileName);
+    const diff = genDiff(beforePath, afterPath, 'plain');
+    expect(diff).toEqual(expected);
+  });
+});
+
+describe('diff data', () => {
+  test('ordered', () => {
+    const beforePath = getFixturePath('before-nested.json');
+    const afterPath = getFixturePath('after-nested.json');
+    const expectedPath = getFixturePath('nested-ordered-diff.json');
     const data1 = JSON.parse(fs.readFileSync(beforePath), 'utf-8');
     const data2 = JSON.parse(fs.readFileSync(afterPath), 'utf-8');
     const expected = JSON.parse(fs.readFileSync(expectedPath, 'utf-8'));
-    const diffData = getDiffData(data1, data2);
+    const diffData = getDiff(data1, data2);
     expect(diffData).toEqual(expected);
   });
 });
 
 // const beforePath = getFixturePath('before-nested.json');
 // const afterPath = getFixturePath('after-nested.json');
-// const data1 = JSON.parse(fs.readFileSync(beforePath), 'utf-8');
-// const data2 = JSON.parse(fs.readFileSync(afterPath), 'utf-8');
-// const diffStructure = getOrderedDiff(data1, data2);
-// console.log(JSON.stringify(diffStructure, null, 2));
+// // const data1 = JSON.parse(fs.readFileSync(beforePath), 'utf-8');
+// // const data2 = JSON.parse(fs.readFileSync(afterPath), 'utf-8');
+// // const diffStructure = getDiff(data1, data2);
+// console.log(genDiff(beforePath, afterPath, 'plain'));

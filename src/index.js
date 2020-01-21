63186e51bcc01c797dc3fdac3fb883b31402bae6
Saitgalin Aleksandr <cred1tor@yandex.ru>
2020-01-21 22:49:31 +0600

    moved formatters to and utils to separate modules

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import _ from 'lodash';
+import treeStringify from './formatters/tree';
+import plainStringify from './formatters/plain';
+import { isObject } from './utils';
 import parse from './parsers';
 
-const isObject = (item) => typeof item === 'object' && item !== null;
-
-const tabSize = 4;
-
-export const getOrderedDiff = (data1, data2) => {
+export const getDiff = (data1, data2) => {
   const keys = _.union(Object.keys(data1), Object.keys(data2));
   const result = [];
 
@@ -16,7 +15,7 @@ export const getOrderedDiff = (data1, data2) => {
     if (_.has(data1, key) && !_.has(data2, key)) {
       result.push({
         key,
-        status: 'deleted',
+        status: 'removed',
         value: value1,
       });
       return;
@@ -44,14 +43,14 @@ export const getOrderedDiff = (data1, data2) => {
       result.push({
         key,
         status: 'nestedDiff',
-        value: getOrderedDiff(value1, value2),
+        value: getDiff(value1, value2),
       });
       return;
     }
 
     result.push({
       key,
-      status: 'changed',
+      status: 'updated',
       oldValue: value1,
       newValue: value2,
     });
@@ -60,51 +59,10 @@ export const getOrderedDiff = (data1, data2) => {
   return result;
 };
 
-export const stringifyOrderedDiff = (diff, depthLevel = 1) => {
-  const indentSize = depthLevel * tabSize;
-  const normalIndent = ' '.repeat(indentSize);
-  const plusIndent = ' '.repeat(indentSize - 2).concat('+ ');
-  const minusIndent = ' '.repeat(indentSize - 2).concat('- ');
-  const higherIndent = ' '.repeat(indentSize - tabSize);
-  const lowerIndent = ' '.repeat(indentSize + tabSize);
-
-  const indent = {
-    unchanged: normalIndent,
-    added: plusIndent,
-    deleted: minusIndent,
-    nestedDiff: normalIndent,
-  };
-
-  const stringifyValue = (data) => {
-    if (!isObject(data)) {
-      return data.toString();
-    }
-
-    const lines = Object.entries(data)
-      .reduce((acc, [key, value]) => [...acc, `${lowerIndent}${key}: ${value}`], []);
-
-    return `{\n${lines.join('\n')}\n${normalIndent}}`;
-  };
-
-  const renderLine = ({
-    key, status, value, oldValue, newValue,
-  }) => {
-    if (status === 'changed') {
-      return `${minusIndent}${key}: ${stringifyValue(oldValue)}\n${plusIndent}${key}: ${stringifyValue(newValue)}`;
-    }
-
-    const valueOutput = status === 'nestedDiff' ? stringifyOrderedDiff(value, depthLevel + 1) : stringifyValue(value);
-    return `${indent[status]}${key}: ${stringifyValue(valueOutput)}`;
-  };
-
-  const lines = diff.map(renderLine);
-
-  return `{\n${lines.join('\n')}\n${higherIndent}}`;
-};
-
-export default (filepath1, filepath2) => {
+export default (filepath1, filepath2, format) => {
   const data1 = parse(filepath1);
   const data2 = parse(filepath2);
-  const diff = getOrderedDiff(data1, data2);
-  return stringifyOrderedDiff(diff);
+  const diff = getDiff(data1, data2);
+  const stringify = format === 'plain' ? plainStringify : treeStringify;
+  return stringify(diff);
 };
---
+++ src/utils.js
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export const isObject = (item) => typeof item === 'object' && item !== null;

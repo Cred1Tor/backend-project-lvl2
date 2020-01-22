9db1516f66e7d44df2b19fe6ea2759ef243edbe3
Saitgalin Aleksandr <cred1tor@yandex.ru>
2020-01-22 23:35:53 +0600

    added describe.each

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,8 +3,12 @@ import genDiff from '../src';
 
 const getFullPath = (fileName) => `${__dirname}/../__fixtures__/${fileName}`;
 
-describe('tree', () => {
-  const expected = fs.readFileSync(getFullPath('diff-nested'), 'utf-8');
+describe.each([
+  ['tree', 'diff-nested'],
+  ['plain', 'plain-diff.txt'],
+  ['json', 'nested-ordered-diff.json'],
+])('%s', (format, expectedFileName) => {
+  const expected = fs.readFileSync(getFullPath(expectedFileName), 'utf-8');
 
   test.each([
     ['json', 'before-nested.json', 'after-nested.json'],
@@ -13,47 +17,7 @@ describe('tree', () => {
   ])('%s', (_testName, beforeFileName, afterFileName) => {
     const beforePath = getFullPath(beforeFileName);
     const afterPath = getFullPath(afterFileName);
-    const diff = genDiff(beforePath, afterPath);
+    const diff = genDiff(beforePath, afterPath, format);
     expect(diff).toEqual(expected);
   });
 });
-
-describe('plain', () => {
-  const expected = fs.readFileSync(getFullPath('plain-diff.txt'), 'utf-8');
-
-  test.each([
-    ['json', 'before-nested.json', 'after-nested.json'],
-    ['yaml', 'before-nested.yml', 'after-nested.yml'],
-    ['ini', 'before-nested.ini', 'after-nested.ini'],
-  ])('%s', (_testName, beforeFileName, afterFileName) => {
-    const beforePath = getFullPath(beforeFileName);
-    const afterPath = getFullPath(afterFileName);
-    const diff = genDiff(beforePath, afterPath, 'plain');
-    expect(diff).toEqual(expected);
-  });
-});
-
-describe('json', () => {
-  const expected = fs.readFileSync(getFullPath('nested-ordered-diff.json'), 'utf-8');
-
-  test.each([
-    ['json', 'before-nested.json', 'after-nested.json'],
-    ['yaml', 'before-nested.yml', 'after-nested.yml'],
-    // ini parser converts numbers into strings, so it doesn't work for json output format
-    // ['ini', 'before-nested.ini', 'after-nested.ini'],
-  ])('%s', (_testName, beforeFileName, afterFileName) => {
-    const beforePath = getFullPath(beforeFileName);
-    const afterPath = getFullPath(afterFileName);
-    const diff = genDiff(beforePath, afterPath, 'json');
-    expect(diff).toEqual(expected);
-  });
-});
-
-// const beforePath = getFullPath('before-nested.ini');
-// const afterPath = getFullPath('after-nested.json');
-// // const data1 = JSON.parse(fs.readFileSync(beforePath), 'utf-8');
-// // const data2 = JSON.parse(fs.readFileSync(afterPath), 'utf-8');
-// // const diffStructure = getDiff(data1, data2);
-// // console.log(genDiff(beforePath, afterPath, 'json'));
-// console.log(parse(beforePath));
-// console.log(parse(afterPath));

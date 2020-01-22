384bd5fe872ea0cd6ba647ed382e8cf93443d910
Saitgalin Aleksandr <cred1tor@yandex.ru>
2020-01-22 10:16:40 +0600

    added tests for json format

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,54 +1,59 @@
 import fs from 'fs';
-import genDiff, { getDiff } from '../src';
+import genDiff from '../src';
 
-const getFixturePath = (fixtureName) => `${__dirname}/../__fixtures__/${fixtureName}`;
+const getFullPath = (fileName) => `${__dirname}/../__fixtures__/${fileName}`;
 
 describe('tree', () => {
-  const expected = fs.readFileSync(getFixturePath('diff-nested'), 'utf-8');
+  const expected = fs.readFileSync(getFullPath('diff-nested'), 'utf-8');
 
   test.each([
     ['json', 'before-nested.json', 'after-nested.json'],
     ['yaml', 'before-nested.yml', 'after-nested.yml'],
     ['ini', 'before-nested.ini', 'after-nested.ini'],
   ])('%s', (_testName, beforeFileName, afterFileName) => {
-    const beforePath = getFixturePath(beforeFileName);
-    const afterPath = getFixturePath(afterFileName);
+    const beforePath = getFullPath(beforeFileName);
+    const afterPath = getFullPath(afterFileName);
     const diff = genDiff(beforePath, afterPath);
     expect(diff).toEqual(expected);
   });
 });
 
 describe('plain', () => {
-  const expected = fs.readFileSync(getFixturePath('plain-diff.txt'), 'utf-8');
+  const expected = fs.readFileSync(getFullPath('plain-diff.txt'), 'utf-8');
 
   test.each([
     ['json', 'before-nested.json', 'after-nested.json'],
     ['yaml', 'before-nested.yml', 'after-nested.yml'],
     ['ini', 'before-nested.ini', 'after-nested.ini'],
   ])('%s', (_testName, beforeFileName, afterFileName) => {
-    const beforePath = getFixturePath(beforeFileName);
-    const afterPath = getFixturePath(afterFileName);
+    const beforePath = getFullPath(beforeFileName);
+    const afterPath = getFullPath(afterFileName);
     const diff = genDiff(beforePath, afterPath, 'plain');
     expect(diff).toEqual(expected);
   });
 });
 
-describe('diff data', () => {
-  test('ordered', () => {
-    const beforePath = getFixturePath('before-nested.json');
-    const afterPath = getFixturePath('after-nested.json');
-    const expectedPath = getFixturePath('nested-ordered-diff.json');
-    const data1 = JSON.parse(fs.readFileSync(beforePath), 'utf-8');
-    const data2 = JSON.parse(fs.readFileSync(afterPath), 'utf-8');
-    const expected = JSON.parse(fs.readFileSync(expectedPath, 'utf-8'));
-    const diffData = getDiff(data1, data2);
-    expect(diffData).toEqual(expected);
+describe('json', () => {
+  const expected = fs.readFileSync(getFullPath('nested-ordered-diff.json'), 'utf-8');
+
+  test.each([
+    ['json', 'before-nested.json', 'after-nested.json'],
+    ['yaml', 'before-nested.yml', 'after-nested.yml'],
+    // eslint-disable-next-line max-len
+    // ['ini', 'before-nested.ini', 'after-nested.ini'], ini parser converts numbers into strings, so it doesn't work for json output format
+  ])('%s', (_testName, beforeFileName, afterFileName) => {
+    const beforePath = getFullPath(beforeFileName);
+    const afterPath = getFullPath(afterFileName);
+    const diff = genDiff(beforePath, afterPath, 'json');
+    expect(diff).toEqual(expected);
   });
 });
 
-// const beforePath = getFixturePath('before-nested.json');
-// const afterPath = getFixturePath('after-nested.json');
+// const beforePath = getFullPath('before-nested.ini');
+// const afterPath = getFullPath('after-nested.json');
 // // const data1 = JSON.parse(fs.readFileSync(beforePath), 'utf-8');
 // // const data2 = JSON.parse(fs.readFileSync(afterPath), 'utf-8');
 // // const diffStructure = getDiff(data1, data2);
-// console.log(genDiff(beforePath, afterPath, 'plain'));
+// // console.log(genDiff(beforePath, afterPath, 'json'));
+// console.log(parse(beforePath));
+// console.log(parse(afterPath));

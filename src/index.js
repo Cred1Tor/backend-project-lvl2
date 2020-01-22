de4aa5e2654c97131d7c6a9a2e8fdaae1b0fbea5
Saitgalin Aleksandr <cred1tor@yandex.ru>
2020-01-22 09:03:38 +0600

    replaced forEach with map in getDiff()

--- src/index.js
+++ src/index.js
@@ -4,65 +4,64 @@ import plainStringify from './formatters/plain';
 import { isObject } from './utils';
 import parse from './parsers';
 
+const format = {
+  tree: treeStringify,
+  plain: plainStringify,
+};
+
 export const getDiff = (data1, data2) => {
   const keys = _.union(Object.keys(data1), Object.keys(data2));
-  const result = [];
 
-  keys.forEach((key) => {
+  const diffs = keys.map((key) => {
     const value1 = data1[key];
     const value2 = data2[key];
 
     if (_.has(data1, key) && !_.has(data2, key)) {
-      result.push({
+      return {
         key,
         status: 'removed',
         value: value1,
-      });
-      return;
+      };
     }
 
     if (!_.has(data1, key) && _.has(data2, key)) {
-      result.push({
+      return {
         key,
         status: 'added',
         value: value2,
-      });
-      return;
+      };
     }
 
     if (value1 === value2) {
-      result.push({
+      return {
         key,
         status: 'unchanged',
         value: value2,
-      });
-      return;
+      };
     }
 
     if (isObject(value1) && isObject(value2)) {
-      result.push({
+      return {
         key,
         status: 'nestedDiff',
         value: getDiff(value1, value2),
-      });
-      return;
+      };
     }
 
-    result.push({
+    return {
       key,
       status: 'updated',
       oldValue: value1,
       newValue: value2,
-    });
+    };
   });
 
-  return result;
+  return diffs;
 };
 
-export default (filepath1, filepath2, format = 'tree') => {
+export default (filepath1, filepath2, formatType = 'tree') => {
   const data1 = parse(filepath1);
   const data2 = parse(filepath2);
   const diff = getDiff(data1, data2);
-  const stringify = format === 'plain' ? plainStringify : treeStringify;
-  return stringify(diff);
+  return format[formatType](diff);
 };

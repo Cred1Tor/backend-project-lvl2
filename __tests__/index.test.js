9c928a79ece2296825ae978390d04fa3d6a61b2f
Saitgalin Aleksandr <cred1tor@yandex.ru>
2020-01-23 11:50:47 +0600

    removed unused fixtures, renamed the others

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,20 +4,28 @@ import genDiff from '../src';
 const getFullPath = (fileName) => `${__dirname}/../__fixtures__/${fileName}`;
 
 describe.each([
-  ['tree', 'diff-nested'],
+  ['tree', 'tree-diff.txt'],
   ['plain', 'plain-diff.txt'],
-  ['json', 'nested-ordered-diff.json'],
+  ['json', 'json-diff.json'],
 ])('%s', (format, expectedFileName) => {
   const expected = fs.readFileSync(getFullPath(expectedFileName), 'utf-8');
 
   test.each([
-    ['json', 'before-nested.json', 'after-nested.json'],
-    ['yaml', 'before-nested.yml', 'after-nested.yml'],
-    ['ini', 'before-nested.ini', 'after-nested.ini'],
-  ])('%s', (_testName, beforeFileName, afterFileName) => {
+    ['json', 'before.json', 'after.json'],
+    ['yaml', 'before.yml', 'after.yml'],
+    ['ini', 'before.ini', 'after.ini'],
+  ])('%s', (testName, beforeFileName, afterFileName) => {
     const beforePath = getFullPath(beforeFileName);
     const afterPath = getFullPath(afterFileName);
     const diff = genDiff(beforePath, afterPath, format);
+
+    // ini parser converts numbers to strings
+    // so the expected result for json format on ini files will be different
+    if (format === 'json' && testName === 'ini') {
+      const expectedNoNumbers = fs.readFileSync(getFullPath('json-diff-no-numbers.json'), 'utf-8');
+      expect(diff).toEqual(expectedNoNumbers);
+    }
+
     expect(diff).toEqual(expected);
   });
 });
